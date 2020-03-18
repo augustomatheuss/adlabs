6fa57c03dcc200a221238bcb38769c0ad0cae881
augustomatheuss <augustomatheuss@ufrn.edu.br>
2020-03-18 00:28:27 -0300

    new version of basic type class in python class basic level

--- python-classes/README.md
+++ python-classes/README.md
@@ -1,5 +1,16 @@
 # PYTHON programming classes guide  
 
+## Content for the BASIC Level
+* Basic Types  
+* Strings  
+* Loops  
+* Compound statements (if, while, for, try, with)
+* Standard input and output
+* Functions  
+* Modules
+* Pseudo-random number (random) and cryptographically strong random numbers (secrets)
+* Input and Output
+
 ## Softwares to install before classes  
 *  [Python 3.8](https://www.python.org/downloads/)  
 *  [PyCharm Community](https://www.jetbrains.com/pycharm/download/)  
--- python-classes/basic_types_class.py
+++ python-classes/basic_types_class.py
@@ -1,50 +1,62 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""
+Basic Types: https://docs.python.org/3/library/stdtypes.html
 
+Principal built-in Types:
+    numerics, sequences, mappings, classes, instances and exceptions.
 
-# Tipos básicos em Python
+Docstrings: https://www.python.org/dev/peps/pep-0257/
+"""
 
+# Comments are for humans!
 
-# Imprimindo mensagem para o usuário.
-print('Olá!')
+#  Numeric Types: integers, floating point numbers, and complex numbers
 
-# Este bloco faz isso.
-# cmd 1
-# cmd 2
-# cmd 3
+# Integer (int)
+age = 12
 
-# Variáveis - Tipo Inteiro
-matricula = 12  # 23 0 14
+# Floating point (float)
+pi = 3.14159265359
 
-# Variáveis - Tipo Flutuante
-pi = 3.14  # 2.0
+# Complex (complex)
+impedance = 3.22+11j
 
-# Variáveis - Tipo Char
-mychar = 'f'  # '0' '*'
-matricula = str(matricula) + '!!'  # DEBUG STEP TRHOUGH
+# List
+fruits = ['laranja', 'maca', 'uva', 'pera']
 
-# Variáveis - Tipo String
-nome = 'Arvore'  # 'Hello' 'Dia' 'Sol'
+# Tuple
+student = ('name', 'id')
 
-opInt = pi * 10 * 3.14
-# %d é inteiro
-# %f é flutuante
-# %s é string
-st = '!'
-print('O resultado é %f %s' % (pi, st))  # se trocar o %f por %d, é feito um cast de flutuante para inteiro
-print('O resultado é %.2f' % pi)
+# Range: start, stop, step
+from_range_1 = list(range(10))
+from_range_2 = list(range(1, 11))
+from_range_3 = list(range(0, 30, 5))
+from_range_4 = list(range(0, 10, 3))
+from_range_5 = list(range(0, -10, -1))
+from_range_6 = list(range(0))
+from_range_7 = list(range(1, 0))
 
+# Text (str)
+string = 'Strings are immutable sequences of Unicode code points.'
 
-# Operações comuns entre strings.
-# https://docs.python.org/3.3/library/string.html
+# Dict
+numbers = {'one': 1, 'two': 2, 'three': 3}
+two = numbers['two']
 
-# 'A r v o r e'
-#  0 1 2 3 4 5
-print(nome.index('r'))
+# Truth value testing
+#  All objects is true unless
+#   Class defines either a __bool__() method that returns False or a __len__() method that returns zero;
+#   Constants defined to be false: None and False.
+#   Zero of any numeric type: 0, 0.0, 0j, Decimal(0), Fraction(0, 1)
+#   Empty sequences and collections: '', (), [], {}, set(), range(0)
+true = True or 1 or list(range(10))
+false = False and 0 and ''
 
-# Concatenacao de Strings
-nome = nome + ' H'
-print(nome)
+# Built-in types we will not see in this class.
+#   Binary Sequence Types — bytes, bytearray, memoryview.
+#   Set Types — set, frozenset
+#   Object oriented programming related.
 
-# Variáveis - Tipo Boolean
-x = True  # False
+# Put the breakpoint to debub in the next line!
+the_end = True
